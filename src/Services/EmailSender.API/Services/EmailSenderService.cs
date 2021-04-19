0e3e20b248dc612a2db7789416d3dfed841d0ab3
Sebastian <newerdim@onet.pl>
2021-04-19 18:27:22 +0200

    Create EmailSenderService

---
+++ src/Services/EmailSender.API/Services/EmailSenderService.cs
@@ -0,0 +1,26 @@
+﻿using EmailSender.API.Helper;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender.API.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailSenderService(IOptions<SmtpSettings> options)
+        {
+            _smtpSettings = options.Value;
+        }
+
+        public async Task SendAsync(MailMessage message)
+        {
+            using var client = new SmtpClient(_smtpSettings.Hostname, _smtpSettings.Port);
+            await client.SendMailAsync(message);
+        }
+    }
+}
---
+++ src/Services/EmailSender.API/Services/IEmailSenderService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender.API.Services
+{
+    public interface IEmailSenderService
+    {
+        public Task SendAsync(MailMessage message);
+    }
+}
