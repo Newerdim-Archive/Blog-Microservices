6e5894b2dfc8c61feca7fd31258983fcbbcc4343
Sebastian <newerdim@onet.pl>
2021-04-19 18:26:50 +0200

    Create shared queue project

---
+++ src/Building Blocks/Queue/Models/EmailMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace EmailSender.API.Consumers
+{
+    public class EmailMessage
+    {
+        public string From { get; set; }
+
+        public string[] To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
+
