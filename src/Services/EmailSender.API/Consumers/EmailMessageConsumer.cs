0d295b5be7ca020082387e71bad245ab52a99128
Sebastian <newerdim@onet.pl>
2021-04-19 18:51:19 +0200

    Add logger

--- src/Services/EmailSender.API/Consumers/EmailMessageConsumer.cs
+++ src/Services/EmailSender.API/Consumers/EmailMessageConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using EmailSender.API.Services;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -9,10 +10,12 @@ namespace EmailSender.API.Consumers
     public class EmailMessageConsumer : IConsumer<EmailMessage>
     {
         private readonly IEmailSenderService _emailSender;
+        private readonly ILogger<EmailMessageConsumer> _logger;
 
-        public EmailMessageConsumer(IEmailSenderService emailSender)
+        public EmailMessageConsumer(IEmailSenderService emailSender, ILogger<EmailMessageConsumer> logger)
         {
             _emailSender = emailSender;
+            _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<EmailMessage> context)
@@ -21,19 +24,21 @@ namespace EmailSender.API.Consumers
             
             if (data is null)
             {
+                _logger.LogInformation($"Empty email message. {context}");
                 return;
             }
 
             if (string.IsNullOrWhiteSpace(data.From))
             {
-                throw new ArgumentException($"From: {data.From} is invalid email.");
+                throw new ArgumentException($"Email 'from' is null or empty.");
             }
 
             foreach(var to in data.To)
             {
                 if (string.IsNullOrWhiteSpace(to))
                 {
-                    throw new ArgumentException($"To: {to} is invalid email.");
+                    _logger.LogWarning($"Email target is null or empty.");
+                    continue;
                 }
 
                 var message = new MailMessage(data.From, to, data.Subject, data.Body);
