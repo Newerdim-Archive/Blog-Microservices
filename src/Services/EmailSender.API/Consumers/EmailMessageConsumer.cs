5e174603bfd1770f072671d100e2552151bc9b87
Sebastian <newerdim@onet.pl>
2021-04-19 18:27:11 +0200

    Create EmailMessageConsumer

---
+++ src/Services/EmailSender.API/Consumers/EmailMessageConsumer.cs
@@ -0,0 +1,44 @@
+﻿using EmailSender.API.Services;
+using MassTransit;
+using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender.API.Consumers
+{
+    public class EmailMessageConsumer : IConsumer<EmailMessage>
+    {
+        private readonly IEmailSenderService _emailSender;
+
+        public EmailMessageConsumer(IEmailSenderService emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<EmailMessage> context)
+        {
+            var data = context.Message;
+            
+            if (data is null)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(data.From))
+            {
+                throw new ArgumentException($"From: {data.From} is invalid email.");
+            }
+
+            foreach(var to in data.To)
+            {
+                if (string.IsNullOrWhiteSpace(to))
+                {
+                    throw new ArgumentException($"To: {to} is invalid email.");
+                }
+
+                var message = new MailMessage(data.From, to, data.Subject, data.Body);
+                await _emailSender.SendAsync(message);
+            }
+        }
+    }
+}
