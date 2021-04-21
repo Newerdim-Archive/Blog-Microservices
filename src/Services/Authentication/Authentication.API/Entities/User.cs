d8b00f3a76f80fc67e749c40679954df0f24a700
Sebastian <newerdim@onet.pl>
2021-04-21 16:50:23 +0200

    Add Entitites

---
+++ src/Services/Authentication/Authentication.API/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Entities
+{
+    public record User : BaseEntity
+    {
+        public string Username { get; init; }
+        public string Email { get; init; }
+        public byte[] PasswordHash { get; init; }
+        public byte[] PasswordSalt { get; init; }
+        public bool ConfirmedEmail { get; init; }
+    }
+}
