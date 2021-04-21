15b8effde97a90040e66723938c22b2b259dede4
Sebastian <newerdim@onet.pl>
2021-04-21 19:48:58 +0200

    Add db connection

---
+++ src/Services/Authentication/Authentication.API/Extensions/ConfigureServicesExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Authentication.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+
+namespace Authentication.API.Extensions
+{
+    public static class ConfigureServicesExtensions
+    {
+        public static void AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddDbContextPool<AuthDataContext>(
+                opt => opt
+                    .UseMySql(
+                        connectionString,
+                        ServerVersion.AutoDetect(connectionString),
+                        mySqlOptions => mySqlOptions
+                            .CharSetBehavior(CharSetBehavior.NeverAppend))
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
+            );
+        }
+    }
+}
