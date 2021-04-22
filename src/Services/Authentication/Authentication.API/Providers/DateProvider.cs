aa7d3a6eb3d04b143bb5232a0eed9b2e30b904dd
Sebastian <newerdim@onet.pl>
2021-04-22 13:54:54 +0200

    Add DateProvider

---
+++ src/Services/Authentication/Authentication.API/Providers/DateProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Providers
+{
+    public class DateProvider : IDateProvider
+    {
+        public DateTimeOffset GetUtcNow()
+        {
+            return DateTimeOffset.UtcNow;
+        }
+    }
+}
