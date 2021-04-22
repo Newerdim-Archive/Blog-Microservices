2afd42df644a281c4c19589203c460a8fc14c5be
Sebastian <newerdim@onet.pl>
2021-04-22 12:33:33 +0200

    Add mapper profile

--- src/Services/Authentication/Authentication.API/Helpers/MapperProfile.cs
+++ src/Services/Authentication/Authentication.API/Helpers/MapperProfile.cs
@@ -1,1 +1,18 @@
-            CreateMap<RegisterRequest, User>();
+﻿using Authentication.API.Dtos;
+using Authentication.API.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+
+        }
+    }
+}
