748aa92c012d5fde93e0ef39fbb79bce06bdefa1
Sebastian <newerdim@onet.pl>
2021-04-24 19:40:44 +0200

    Add TokenService

---
+++ src/Services/Authentication/Authentication.API/Helpers/CustomClaimTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Helpers
+{
+    public class CustomClaimTypes
+    {
+        public const string UserId = "uid";
+        public const string Reason = "res";
+    }
+}
