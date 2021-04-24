dcd80fbcdc8dc5d617e2a93d9f06132ab9e1c05c
Sebastian <newerdim@onet.pl>
2021-04-24 15:50:10 +0200

    Add GetAfterUtcNow

--- src/Services/Authentication/Authentication.API/Providers/DateProvider.cs
+++ src/Services/Authentication/Authentication.API/Providers/DateProvider.cs
@@ -7,6 +7,11 @@ namespace Authentication.API.Providers
 {
     public class DateProvider : IDateProvider
     {
+        public DateTimeOffset GetAfterUtcNow(int days, int minutes)
+        {
+            return DateTimeOffset.UtcNow.AddDays(days).AddMinutes(minutes);
+        }
+
         public DateTimeOffset GetUtcNow()
         {
             return DateTimeOffset.UtcNow;
--- src/Services/Authentication/Authentication.API/Providers/IDateProvider.cs
+++ src/Services/Authentication/Authentication.API/Providers/IDateProvider.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Authentication.API.Providers
 {
     public interface IDateProvider
     {
+        /// <summary>
+        /// Get UTC now date
+        /// </summary>
+        /// <returns>UTC now date</returns>
         DateTimeOffset GetUtcNow();
+
+        /// <summary>
+        /// Get UTC date after specified time
+        /// </summary>
+        /// <param name="days">How many days after</param>
+        /// <param name="minutes">How many minutes after</param>
+        /// <returns>UTC now with added days and minutes</returns>
+        DateTimeOffset GetAfterUtcNow(int days, int minutes);
     }
 }
