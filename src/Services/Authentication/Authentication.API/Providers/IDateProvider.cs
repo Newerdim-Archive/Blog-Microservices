2696e8f15e3066efee927b81b38368ae8460de64
Sebastian <newerdim@onet.pl>
2021-04-26 16:12:01 +0200

    Update comments in IDateProvider

--- src/Services/Authentication/Authentication.API/Providers/IDateProvider.cs
+++ src/Services/Authentication/Authentication.API/Providers/IDateProvider.cs
@@ -16,6 +16,7 @@ namespace Authentication.API.Providers
         /// <param name="days">How many days after</param>
         /// <param name="minutes">How many minutes after</param>
         /// <returns>UTC now with added days and minutes</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when number of days is too big</exception>
         DateTimeOffset GetAfterUtcNow(int days, int minutes);
     }
 }
