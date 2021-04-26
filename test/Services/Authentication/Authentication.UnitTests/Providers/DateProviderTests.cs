2db1f1118376e5fdda20347651f5845989ecd541
Sebastian <newerdim@onet.pl>
2021-04-26 16:12:15 +0200

    Add tests for DateProvider

---
+++ test/Services/Authentication/Authentication.UnitTests/Providers/DateProviderTests.cs
@@ -0,0 +1,65 @@
+﻿using Authentication.API.Providers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Authentication.UnitTests.Providers
+{
+    public class DateProviderTests
+    {
+        private static IDateProvider CreateProvider()
+        {
+            return new DateProvider();
+        }
+
+        [Fact]
+        public void GetUtcNow_ReturnsDateNow()
+        {
+            // Arrange
+            var sut = CreateProvider();
+            var exceptedDate = DateTimeOffset.UtcNow;
+
+            // Act
+            var result = sut.GetUtcNow();
+
+            // Assert
+            result.Should().BeCloseTo(exceptedDate);
+        }
+
+        [Fact]
+        public void GetAfterUtcNow_ValidData_ReturnsDateNow()
+        {
+            // Arrange
+            var sut = CreateProvider();
+            var exceptedDate = DateTimeOffset.UtcNow
+                .AddDays(1)
+                .AddMinutes(4);
+
+            // Act
+            var result = sut.GetAfterUtcNow(1, 4);
+
+            // Assert
+            result.Should().BeCloseTo(exceptedDate);
+        }
+
+        [Fact]
+        public void GetAfterUtcNow_TooBigNumberOfDays_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            var sut = CreateProvider();
+            var exceptedDate = DateTimeOffset.UtcNow
+                .AddDays(1)
+                .AddMinutes(4);
+
+            // Act
+            Action act = () => sut.GetAfterUtcNow(int.MaxValue, 4);
+
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
