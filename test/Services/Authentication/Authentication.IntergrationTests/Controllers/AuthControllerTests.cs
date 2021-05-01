00476f9caa7b2b548530d11efafb6d8ced9ed808
Sebastian <newerdim@onet.pl>
2021-05-01 11:29:34 +0200

    Create auth integration tests

--- test/Services/Authentication/Authentication.IntergrationTests/Controllers/AuthControllerTests.cs
+++ test/Services/Authentication/Authentication.IntergrationTests/Controllers/AuthControllerTests.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,34 @@ namespace Authentication.IntergrationTests.Controllers
 {
     class AuthControllerTests
     {
+
+        private static string CreateValidUsername()
+        {
+            var fixture = new Fixture();
+
+            var usernamePattern = @"^[A-Za-z0-9]{3}(?:[ _-][A-Za-z0-9]+){6}$";
+            var username = new SpecimenContext(fixture)
+                .Resolve(new RegularExpressionRequest(usernamePattern));
+
+            return username.ToString().Substring(0, 20);
+        }
+
+        private static string CreateValidPassword()
+        {
+            var fixture = new Fixture();
+
+            var passwordPattern = @"^[A-Z]{3}[a-z]{3}[0-9]{3}$";
+            var password = new SpecimenContext(fixture)
+                .Resolve(new RegularExpressionRequest(passwordPattern));
+
+            return password.ToString();
+        }
+
+        private static string CreateValidEmail()
+        {
+            var fixture = new Fixture();
+
+            return fixture.Create<MailAddress>().Address;
+        }
     }
 }
