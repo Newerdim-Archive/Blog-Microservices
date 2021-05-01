3c01d392775f1634019bde76b9521f07dec8557e
Sebastian <newerdim@onet.pl>
2021-05-01 11:28:04 +0200

    Create UserPublisher interface

---
+++ src/Services/Authentication/Authentication.API/Dtos/PublishNewUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Dtos
+{
+    public class PublishNewUserRequest
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ src/Services/Authentication/Authentication.API/Publishers/IUserPublisher.cs
@@ -0,0 +1,15 @@
+﻿using Authentication.API.Dtos;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Publishers
+{
+    public interface IUserPublisher
+    {
+        /// <summary>
+        /// Publish newly created user
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task PublishNewUser(PublishNewUserRequest request);
+    }
+}
