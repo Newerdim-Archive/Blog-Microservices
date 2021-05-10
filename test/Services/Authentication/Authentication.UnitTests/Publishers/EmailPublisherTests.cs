38720fe3fc4b9af0d3e5aa871f2b12f1bc65e1f2
Sebastian <newerdim@onet.pl>
2021-05-10 15:38:02 +0200

    Add Tests for PublishEmailConfirmation

--- test/Services/Authentication/Authentication.UnitTests/Publishers/EmailPublisherTests.cs
+++ test/Services/Authentication/Authentication.UnitTests/Publishers/EmailPublisherTests.cs
@@ -1,17 +1,20 @@
-﻿using Authentication.API.Helpers;
+﻿using Authentication.API.Dtos;
+using Authentication.API.Helpers;
 using Authentication.API.Publishers;
+using EventBus.Events;
+using FluentAssertions;
 using MassTransit;
 using Microsoft.Extensions.Options;
 using Moq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Authentication.UnitTests.Publishers
 {
-    class EmailPublisherTests
+    public class EmailPublisherTests
     {
         private readonly Mock<IOptions<CompanySettings>> _companySettingsOptionsMock = new();
         private readonly Mock<IPublishEndpoint> _publishEndpointMock = new();
@@ -35,5 +38,89 @@ namespace Authentication.UnitTests.Publishers
         }
 
 
+        [Fact]
+        public async Task PublishEmailConfirmationAsync_ValidRequest_PublishEvent()
+        {
+            // Arrange
+            var request = new PublishEmailConfirmationRequest
+            {
+                TargetEmail = "target@mail.com",
+                Token = "secretToken",
+                ReturnUrl = "www.website.com/email-confrimation"
+            };
+
+            var sut = CreatePublisher();
+
+            // Act
+            await sut.PublishEmailConfirmationAsync(request);
+
+            // Assert
+            _publishEndpointMock.Verify(x => x
+                .Publish(It.IsAny<SendEmailEvent>(), It.IsAny<CancellationToken>()), 
+                    Times.Once);
+        }
+
+        [Fact]
+        public async Task PublishEmailConfirmationAsync_ValidRequest_PublishValidEmail()
+        {
+            // Arrange
+            var targetEmail = "target@mail.com";
+            var token = "secretToken";
+            var returnUrl = "www.website.com/email-confrimation";
+
+            var sut = CreatePublisher();
+
+            // Act
+            await sut.PublishEmailConfirmationAsync(new PublishEmailConfirmationRequest
+            {
+                TargetEmail = targetEmail,
+                Token = token,
+                ReturnUrl = returnUrl
+            });
+
+            // Assert
+            _publishEndpointMock.Verify(x => 
+                x.Publish(
+                    It.Is<SendEmailEvent>(e => 
+                        e.To.Contains(targetEmail) &&
+                        e.Subject.Contains("email verification") &&
+                        e.Body.Contains(token) &&
+                        e.From.Contains("test@company.com")), 
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task PublishEmailConfirmationAsync_InvalidRequest_ThrowsArgumentException()
+        {
+            // Arrange
+            var request = new PublishEmailConfirmationRequest
+            {
+                TargetEmail = "",
+                Token = "",
+                ReturnUrl = ""
+            };
+
+            var sut = CreatePublisher();
+
+            // Act
+            Func<Task> act = () => sut.PublishEmailConfirmationAsync(request);
+
+            // Assert
+            await act.Should().ThrowAsync<ArgumentException>();
+        }
+
+        [Fact]
+        public async Task PublishEmailConfirmationAsync_NullRequest_ThrowsArgumentException()
+        {
+            // Arrange
+            var sut = CreatePublisher();
+
+            // Act
+            Func<Task> act = () => sut.PublishEmailConfirmationAsync(null);
+
+            // Assert
+            await act.Should().ThrowAsync<ArgumentException>();
+        }
     }
 }
