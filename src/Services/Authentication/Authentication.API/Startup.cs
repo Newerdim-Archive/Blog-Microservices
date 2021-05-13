bf4e4be7b1d4996f104651841c3bc4b6ca5fa302
Sebastian <newerdim@onet.pl>
2021-05-13 13:52:07 +0200

    Add auto migration and redirect to swagger

--- src/Services/Authentication/Authentication.API/Startup.cs
+++ src/Services/Authentication/Authentication.API/Startup.cs
@@ -1,3 +1,4 @@
+using Authentication.API.Data;
 using Authentication.API.Extensions;
 using Authentication.API.Validators;
 using FluentValidation.AspNetCore;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -56,8 +58,10 @@ namespace Authentication.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AuthDataContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -73,6 +77,12 @@ namespace Authentication.API
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("", context =>
+                {
+                    context.Response.Redirect("/swagger/");
+                    return Task.FromResult(0);
+                });
+
                 endpoints.MapControllers();
             });
         }
