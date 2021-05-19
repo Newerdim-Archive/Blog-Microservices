6bf6bd72d503d5a7095cc10492d63e183df01e0d
Sebastian <newerdim@onet.pl>
2021-05-19 16:58:07 +0200

    Rename to ConsumerResponse

---
+++ src/Building Blocks/EventBus/Responses/ConsumerResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace EventBus.Results
+{
+    public class ConsumerResponse
+    {
+        public bool Successful { get; set; }
+
+        public ConsumerResponse(bool successful = true)
+        {
+            Successful = successful;
+        }
+    }
+}
