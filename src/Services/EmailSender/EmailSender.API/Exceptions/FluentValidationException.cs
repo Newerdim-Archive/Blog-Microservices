b69c1ef8b8c8f3f41e81d5e9b163ae30208089b7
Sebastian <newerdim@onet.pl>
2021-05-20 11:22:46 +0200

    Add summary

--- src/Services/EmailSender/EmailSender.API/Exceptions/FluentValidationException.cs
+++ src/Services/EmailSender/EmailSender.API/Exceptions/FluentValidationException.cs
@@ -5,6 +5,13 @@ namespace EmailSender.API.Exceptions
 {
     public class FluentValidationException : Exception
     {
+        /// <summary>
+        /// Exception for invalid fluent validation
+        /// </summary>
+        /// <param name="testedClass"></param>
+        /// <param name="validationResult"></param>
+        /// <param name="paramName"></param>
+        /// <exception cref="NullReferenceException">Throws when <paramref name="validationResult"/> is null</exception>
         public FluentValidationException(Type testedClass, ValidationResult validationResult, string paramName)
             : base($"{testedClass} is invalid. Errors: {string.Concat(validationResult.Errors)}. Parameter name: {paramName}")
         {
