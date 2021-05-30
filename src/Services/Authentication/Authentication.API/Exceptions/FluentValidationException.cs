91ae4d784389ee4d573e06eff5922f731c781b89
Sebastian <newerdim@onet.pl>
2021-05-30 12:01:08 +0200

    Add FluentValidationException

---
+++ src/Services/Authentication/Authentication.API/Exceptions/FluentValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace EmailSender.API.Exceptions
+{
+    public class FluentValidationException : Exception
+    {
+        /// <summary>
+        /// Exception for invalid fluent validation
+        /// </summary>
+        /// <param name="testedClass"></param>
+        /// <param name="validationResult"></param>
+        /// <param name="paramName"></param>
+        /// <exception cref="NullReferenceException">Throws when <paramref name="validationResult"/> is null</exception>
+        public FluentValidationException(Type testedClass, ValidationResult validationResult, string paramName)
+            : base($"{testedClass} is invalid. Errors: {string.Concat(validationResult.Errors)}. Parameter name: {paramName}")
+        {
+        }
+    }
+}
